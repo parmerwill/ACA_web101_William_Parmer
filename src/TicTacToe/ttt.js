8bf365b2c59a5c3074f9bca8e02beafe8f148393
parmerwill <parmerwill395@gmail.com>
2020-03-03 18:54:44 -0600

    a

---
+++ src/TicTacToe/ttt.js
@@ -0,0 +1,13 @@
+var math = 1+10; var science='cool'
+console.log('Hey! Im Will')
+
+// learn how to get an element from the DOM//
+var mainContainer = document.getElementsByClassName('main-container')
+var mainContainerQuery =document.querySelector('.main-container')
+console.log ('mainContainer', mainContainer)
+console.log ('mainContainerQuery', mainContainerQuery)
+mainContainer[0].style.height='100px';
+mainContainer[0].style.width='500px';
+mainContainer[0].style.border='1px solid red'
+
+
